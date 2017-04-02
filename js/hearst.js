572e4e0ef3439c0198b6de2f0966f4e87bc14b4a
gabriellecalise <gcalise@ufl.edu>
2017-04-02 17:51:54 -0400

    first commit

--- js/hearst.js
+++ js/hearst.js
@@ -7,3 +7,25 @@ jamModal.querySelector('.modal-close').addEventListener('click',function(){
 document.getElementById('click').addEventListener('click',function(){
 		jamModal.style.display = 'block';
 })
+
+
+var maudesModal;
+maudesModal = document.getElementById('maudesModal');
+maudesModal.querySelector('.modal-close').addEventListener('click', function() {
+  maudesModal.style.display = 'none';
+});
+
+document.getElementById('clickMaudes').addEventListener('click', function() {
+  maudesModal.style.display = 'block';
+})
+
+
+var doughModal;
+doughModal = document.getElementById('doughModal');
+doughModal.querySelector('.modal-close').addEventListener('click', function() {
+  doughModal.style.display = 'none';
+});
+
+document.getElementById('clickDough').addEventListener('click', function() {
+  doughModal.style.display = 'block';
+})
